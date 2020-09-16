4da962f1a10def6a2f903228b6e00bdfd4197c7b
Micaela Acuña <micahl1981@gmail.com>
2020-09-15 22:10:51 -0300

    bubble sort func

--- Ejercicio 19 - BUBBLE SORT func/main.c
+++ Ejercicio 19 - BUBBLE SORT func/main.c
@@ -1,114 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 4
-
 /*
-1_ Suma Acumulada
-2_ Mayor Edad
-3_ Menor edad
-4_ Cant mayor edad repetida
-5_ Cant menor edad repetida
-6_ Promedio
+*
+TAREA:
+modificar la función para que sea una función que ordene numeros enteros
+PERO el criteorio de ordenamieto se lo tengo que pasar yo
+
+trucazo: si le paso un 1 que sea ascendente, 0 descendente
+*
 */
 
-int sumarVector(int vec[], int tam);
-int mayorNum(int vec[], int tam);
-int menorNum(int vec[], int tam);
-int contenidoRepetido(int vec[], int tam, int r);
-float promedioVector(int vec[], int tam);
+int bubleSort(int vec[], int tam, int condicion);
 
 int main()
 {
-    int ojete[] = {24, 30, 24, 25};
-    int suma;
-    int mayor;
-    int menor;
-    float prom;
-
-    // Suma
-    suma = sumarVector(ojete, TAM);
-    printf("1. La suma del contenido del vector es: %d\n", suma);
-
-    // Mayor
-    mayor = mayorNum(ojete, TAM);
-    printf("2. El numero mayor es: %d\n", mayor);
-
-    // Menor
-    menor = menorNum(ojete, TAM);
-    printf("3. El numero menor es: %d\n", menor);
-
-    // Cuantas veces se repite el mayor
-    printf("4. Cantidad de repeticiones del numero mayor: %d\n", contenidoRepetido(ojete, TAM, mayor));
-
-    // Cuantas veces se repite el menor
-    printf("5. Cantidad de repeticiones del numero menor: %d\n", contenidoRepetido(ojete, TAM, menor));
-
-    // Promedio
-    prom = promedioVector(ojete, TAM);
-    printf("6. El promedio de los elementos del vector es: %.2f\n", prom);
+    int coso[] = {6, 4, 5, 2, 3, 1};
+    bubleSort(coso, 6, 1);
+    bubleSort(coso, 6, 0);
+    bubleSort(coso, 6, 4);
 
     return 0;
 }
 
-int sumarVector(int vec[], int tam)
-{
-    int suma = 0;
-    for(int i = 0; i < tam; i++)
-    {
-        suma += vec[i];
-    }
-    return suma;
-}
 
-int mayorNum(int vec[], int tam)
+int bubleSort(int vec[], int tam, int condicion)
 {
-    int mayor;
-    for(int i = 0; i < tam; i++)
+    int error = 0;
+    int aux;
+    if(condicion == 1)
     {
-        if(mayor == 0 || mayor <= vec[i])
+        for(int i = 0; i < tam - 1; i++)
         {
-            mayor = vec[i];
+            for(int j = i + 1; j < tam; j++)
+            {
+                if(vec[i] > vec[j])
+                {
+                    aux = vec[i];
+                    vec[i] = vec[j];
+                    vec[j] = aux;
+                }
+            }
+        }
+
+        printf("\nEl vector ordenado en forma creciente queda:\n");
+        for(int i = 0; i < tam; i++)
+        {
+            printf("%d ", vec[i]);
         }
     }
-    return mayor;
-}
-
-int menorNum(int vec[], int tam)
-{
-    int menor;
-    for(int i = 0; i < tam; i++)
+    else if(condicion == 0)
     {
-        if(menor == 0 || menor >= vec[i])
+        for(int i = 0; i < tam - 1; i++)
         {
-            menor = vec[i];
+            for(int j = i + 1; j < tam; j++)
+            {
+                if(vec[i] < vec[j])
+                {
+                    aux = vec[i];
+                    vec[i] = vec[j];
+                    vec[j] = aux;
+                }
+            }
         }
-    }
-    return menor;
-}
-
-int contenidoRepetido(int vec[], int tam, int r)
-{
-    int repeticiones = 0;
-    for(int i = 0; i < tam; i++)
-    {
-        if(vec[i] == r)
+        printf("\nEl vector ordenado en forma decreciente queda:\n");
+        for(int i = 0; i < tam; i++)
         {
-            repeticiones++;
+            printf("%d ", vec[i]);
         }
-    }
-    return repeticiones;
-}
-
-float promedioVector(int vec[], int tam){
-    int suma = 0;
-    float prom;
-    for(int i = 0; i < tam; i++)
-    {
-        suma += vec[i];
+    } else {
+    error = 1;
+    printf("\nAlgo salio mal...\n");
     }
 
-    prom = (float) suma / tam;
-
-    return prom;
+    return error;
 }
