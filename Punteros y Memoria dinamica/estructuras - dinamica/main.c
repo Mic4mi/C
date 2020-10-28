0482eca3a3b4e70d99e37bacd53578b890d6e3f9
Micaela Acuña <micahl1981@gmail.com>
2020-10-28 19:16:29 -0300

    Dinamica

--- Punteros y Memoria dinamica/estructuras - dinamica/main.c
+++ Punteros y Memoria dinamica/estructuras - dinamica/main.c
@@ -11,16 +11,25 @@ typedef struct
 } eEmpleado;
 
 int mostrarEmpleado(eEmpleado* pEmp);
+eEmpleado* newEmpleado();
+eEmpleado* newEmpleadoParam(int legajo, char* nombre, char sexo, float sueldo);
+//validar
+int empleado_setLegajo(eEmpleado* pEmp, int legajo);
+int empleado_setSueldo(eEmpleado* pEmp, float sueldo);
+int empleado_setSexo(eEmpleado* pEmp, char sexo);
+int empleado_setNombre(eEmpleado* pEmp, char*nombre);
 
 int main()
 {
-    eEmpleado* pEmp1 = (eEmpleado*) malloc(sizeof(eEmpleado));
+    eEmpleado* pEmp1 = newEmpleado();
+    eEmpleado* pEmp2 = newEmpleadoParam(3333, "Julia", 'f', 62000);
 
-    if(pEmp1 == NULL)
+    if(pEmp1 == NULL && pEmp2 != NULL)
     {
         printf("\nNo se pudo obtener memoria.\n");
         exit(EXIT_FAILURE);
     }
+
 
     //CARGAR ESTRUCTURA
     pEmp1->legajo = 1234;
@@ -29,6 +38,7 @@ int main()
     pEmp1->sueldo = 47265.78;
 
     mostrarEmpleado(pEmp1);
+    mostrarEmpleado(pEmp2);
 
     free(pEmp1);
     return 0;
@@ -53,6 +63,116 @@ int mostrarEmpleado(eEmpleado* pEmp)
 
         printf("\n\n");
 
+        error = 0;
+    }
+
+    return error;
+}
+
+/*
+función constructrora
+devuelve un puntero a una nueva estructura empleado vacia
+*/
+
+eEmpleado* newEmpleado()
+{
+    eEmpleado* nuevoEmpleado = NULL;
+    nuevoEmpleado = (eEmpleado*) malloc(sizeof(eEmpleado));
+
+    if(nuevoEmpleado != NULL)
+    {
+        nuevoEmpleado->legajo = 0;
+        strcpy(nuevoEmpleado->nombre, "");
+        nuevoEmpleado->sexo = ' ';
+        nuevoEmpleado->sueldo = 0;
+    }
+    return nuevoEmpleado;
+}
+
+/*
+función constructrora con parametros
+devuelve un puntero a una nueva estructura empleado cargada
+*/
+
+eEmpleado* newEmpleadoParam(int legajo, char* nombre, char sexo, float sueldo)
+{
+    eEmpleado* nuevoEmpleado = newEmpleado();
+
+    if(nuevoEmpleado != NULL)
+    {
+
+        if(
+            !(!empleado_setLegajo(nuevoEmpleado, legajo) &&
+              !empleado_setNombre(nuevoEmpleado, nombre) &&
+              !empleado_setSexo(nuevoEmpleado, sexo) &&
+              !empleado_setSueldo(nuevoEmpleado, sueldo))
+        )
+        {
+            nuevoEmpleado = NULL;
+        }
+    }
+
+    return nuevoEmpleado;
+}
+
+
+/*
+
+Validaciones!!!!
+
+getter -> Leer. Obtiene datos
+
+setter -> Escribir
+
+
+*/
+
+int empleado_setLegajo(eEmpleado* pEmp, int legajo)
+{
+    int error = -1;
+
+    if(pEmp != NULL && legajo > 0)
+    {
+        pEmp->legajo = legajo;
+        error = 0;
+    }
+
+    return error;
+}
+
+int empleado_setSueldo(eEmpleado* pEmp, float sueldo)
+{
+    int error = -1;
+
+    if(pEmp != NULL && sueldo > 0)
+    {
+        pEmp->sueldo = sueldo;
+        error = 0;
+    }
+
+    return error;
+}
+
+int empleado_setSexo(eEmpleado* pEmp, char sexo)
+{
+    int error = -1;
+
+    if(pEmp != NULL && (sexo == 'm' || sexo == 'f'))
+    {
+        pEmp->sexo = sexo;
+        error = 0;
+    }
+
+    return error;
+}
+
+int empleado_setNombre(eEmpleado* pEmp, char*nombre)
+{
+    int error = -1;
+
+    if(pEmp != NULL && nombre != NULL) //Profundizar validacion
+    {
+        strcpy(pEmp->nombre, nombre);
         error = 0;
     }
 
