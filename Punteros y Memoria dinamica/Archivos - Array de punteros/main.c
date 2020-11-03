fb4b49d86a972148c84bd2e769aa04b7ee7322a1
Micaela Acuña <micahl1981@gmail.com>
2020-11-03 12:30:40 -0300

    Inicializar, mostrar

--- Punteros y Memoria dinamica/Archivos - Array de punteros/main.c
+++ Punteros y Memoria dinamica/Archivos - Array de punteros/main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-typedef struct{
+typedef struct
+{
     int legajo;
     char nombre[21];
     char sexo;
     float altura;
-}ePersona;
+} ePersona;
 
 int personas_inicializar(ePersona** lista, int tam);
+int personas_buscarLibre(int* pIndice, ePersona** lista, int tam);
+int personas_mostrarItem(ePersona* persona);
+int personas_mostrarLista(ePersona** lista, int tam);
 
 int main()
 {
     int tam = 5;
-    ePersona** lista = (ePersona**) malloc(tam * sizeof(ePersona*));
+    int indiceLibre;
+
+    ePersona** personas = (ePersona**) malloc(tam * sizeof(ePersona*));
+    ePersona* auxPersona;
 
     /*
     lista es un puntero a puntero, ya que apunta a la dirección
@@ -25,16 +34,139 @@ int main()
     100PRE que declaramos un puntero hay que inicializarlo en NULL
     */
 
-    if(lista == NULL){
+    if(personas == NULL)
+    {
         printf("\nNo se pudo conseguir memoria...\n");
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); //SOLO SE USA EXIT EN MAIN, NO EN OTRAS FUNCIONES!!!
     }
 
-    for(int i = 0; i < tam; i++){
-        *(lista + i) = NULL;
-        //(lista+i) hace referencia al primer elemento del array.
-        // con * adelanrte hacemos referencia al valor de lon que hay en esa posición
+    //inicializar
+    if(!personas_inicializar(personas, tam))
+    {
+        printf("\nInicializacion exitosa.\n");
+    }
+    else
+    {
+        printf("\nProblemas al inicializar personas.\n");
+    }
+
+    //buscar libre
+    if(personas_buscarLibre(&indiceLibre, personas, tam))
+    {
+        printf("\nAlguno de los punteros es null\n");
+    }
+
+    if(indiceLibre == -1)
+    {
+        printf("\nNo hay mas lugar....\n"); //Acá habría que hacer un realloc.
+    }
+    else
+    {
+        printf("\n%d\n", indiceLibre);
+        auxPersona = (ePersona*) malloc(sizeof(ePersona));
+        if(auxPersona == NULL)
+        {
+            printf("\nNo se pudo conseguir espacio en la memoria.\n");
+        }
+        else
+        {
+            auxPersona->legajo = 1234;
+            strcpy(auxPersona->nombre, "Pepe");
+            auxPersona->sexo = 'm';
+            auxPersona->altura = 1.88;
+
+            *(personas + indiceLibre) = auxPersona;
+
+            printf("\nAlta exitosa\n");
+            personas_mostrarLista(personas, tam);
+        }
     }
 
     return 0;
 }
+
+int personas_inicializar(ePersona** lista, int tam)
+{
+    int error = -1;
+
+    if(lista != NULL && tam > 0)
+    {
+        for(int i = 0; i < tam; i++)
+        {
+            *(lista + i) = NULL;
+            //(lista+i) hace referencia al primer elemento del array.
+            // con * adelanrte hacemos referencia al valor de lon que hay en esa posición
+        }
+        error = 0;
+    }
+
+    return error;
+}
+
+int personas_buscarLibre(int* pIndice, ePersona** lista, int tam)
+{
+    int error = -1;
+
+    if(lista != NULL && pIndice != NULL && tam > 0)
+    {
+        *pIndice = -1; //Si no se logró cargar el dato porque el vector esta lleno
+        for(int i = 0; i < tam; i++)
+        {
+            if(*(lista + i) == NULL)
+            {
+                *pIndice = i;
+                break;
+            }
+        }
+        error = 0;
+    }
+
+    return error;
+}
+
+int personas_mostrarItem(ePersona* persona)
+{
+    int error = -1;
+
+    if(persona != NULL)
+    {
+        printf(" %d   %10s       %c       %.2f\n",
+               persona->legajo,
+               persona->nombre,
+               persona->sexo,
+               persona->altura
+              );
+
+        error = 0;
+    }
+
+    return error;
+}
+
+int personas_mostrarLista(ePersona** lista, int tam)
+{
+    int error = -1;
+    int flag = 0;
+
+    if(lista != NULL && tam > 0)
+    {
+        printf("\n ID         NOMBRE      SEXO    ALTURA\n");
+        for(int i = 0; i < tam; i++)
+        {
+            if(*(lista + i ) != NULL)
+            {
+                personas_mostrarItem(*(lista + i));
+                flag = 1;
+            }
+        }
+
+        if(flag == 0)
+        {
+            printf("\nNo hay personas para mostrar.\n");
+        }
+
+        error = 0;
+    }
+    return error;
+}
+
