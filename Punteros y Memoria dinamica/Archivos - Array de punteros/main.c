b1500bd04a8fa39e7d43195e8a4a13e30780f50c
Micaela Acuña <micahl1981@gmail.com>
2020-11-03 13:19:46 -0300

    destroy persona

--- Punteros y Memoria dinamica/Archivos - Array de punteros/main.c
+++ Punteros y Memoria dinamica/Archivos - Array de punteros/main.c
@@ -18,11 +18,19 @@ int personas_mostrarLista(ePersona** lista, int tam);
 
 //Contructora
 ePersona* persona_new();
+ePersona* persona_newParams(int id, char* nombre, char sexo, float altura);
+
+//destructora
+int personas_destruir(ePersona* persona);
+
+//buscar
+int personas_buscar(int* pIndice, int id, ePersona** lista, int tam);
 
 int main()
 {
     int tam = 5;
     int indiceLibre;
+    int indiceBaja;
 
     ePersona** personas = (ePersona**) malloc(tam * sizeof(ePersona*));
     ePersona* auxPersona;
@@ -66,24 +74,34 @@ int main()
     else
     {
         printf("\n%d\n", indiceLibre);
-        auxPersona = persona_new();
+        auxPersona = persona_newParams(1450, "Agustin", 'm', 1.82);
         if(auxPersona == NULL)
         {
             printf("\nNo se pudo conseguir espacio en la memoria.\n");
         }
         else
         {
-            auxPersona->legajo = 1234;
-            strcpy(auxPersona->nombre, "Pepe");
-            auxPersona->sexo = 'm';
-            auxPersona->altura = 1.88;
 
             *(personas + indiceLibre) = auxPersona;
-
             printf("\nAlta exitosa\n");
-            personas_mostrarLista(personas, tam);
         }
     }
+    personas_mostrarLista(personas, tam);
+    printf("\n----------------------------------------------------\n");
+
+    personas_buscar(&indiceBaja, 1450, personas,tam);
+
+    if(indiceBaja == -1)
+    {
+        printf("\nNo hay personas con ese id\n");
+    }
+    else
+    {
+        personas_destruir(*(personas + indiceBaja));
+        *(personas + indiceBaja) = NULL;
+    }
+
+    personas_mostrarLista(personas, tam);
 
     return 0;
 }
@@ -175,6 +193,81 @@ int personas_mostrarLista(ePersona** lista, int tam)
 
 ePersona* persona_new()
 {
-    //Nosotros al invocar esta funcion debemos asegurarnos que devuelva algo distinto de null
-    return (ePersona*) malloc(sizeof(ePersona));
+    ePersona* nuevaPersona;
+    nuevaPersona = (ePersona*) malloc(sizeof(ePersona));
+
+    if(nuevaPersona != NULL)
+    {
+        nuevaPersona->legajo = 0;
+        strcpy(nuevaPersona->nombre, "");
+        nuevaPersona->sexo = ' ';
+        nuevaPersona->altura = 0;
+    }
+
+    return nuevaPersona;
 }
+
+ePersona* persona_newParams(int id, char* nombre, char sexo, float altura)
+{
+    ePersona* nuevaPersona;
+
+    nuevaPersona = persona_new();
+
+    if(nuevaPersona != NULL)
+    {
+        //Cada campo se debe validar con un SETTER
+        nuevaPersona->legajo = id;
+        strcpy(nuevaPersona->nombre, nombre);
+        nuevaPersona->sexo = sexo;
+        nuevaPersona->altura = altura;
+    }
+    /*
+    UNA VEZ QUE SE VALIDA CADA CAMPO CON LOS SETTER,
+    SI LAS COSAS NO SON VALIDAS, SE ENTRA POR ESTE ELSE
+    QUE BASICAMENTE LIBERA LA MEMORIA OCUPADA Y CONVIERTE
+    EL PUNTERO A NULL
+
+    else
+    {
+       free(nuevaPersona);
+       nuevaPersona = NULL;
+    }
+    */
+
+    return nuevaPersona;
+}
+
+int personas_destruir(ePersona* persona)
+{
+    int error = -1;
+
+    if(persona != NULL)
+    {
+        free(persona);
+        error = 0;
+    }
+
+    return error;
+}
+
+int personas_buscar(int* pIndice, int id, ePersona** lista, int tam)
+{
+    int error = -1;
+
+    if(lista != NULL && pIndice != NULL && tam > 0)
+    {
+        *pIndice = -1; //Si no se logró cargar el dato porque no lo econtró
+        for(int i = 0; i < tam; i++)
+        {
+            if(*(lista + i) != NULL && (*(lista + i ))->legajo == id)
+            {
+                *pIndice = i;
+                break;
+            }
+        }
+        error = 0;
+    }
+
+    return error;
+}
+
